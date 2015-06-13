438eed8d3ad9de3215e01245f263961b26d90aac
OhYoungJooung <wnapdlf05@gmail.com>
2015-06-13 17:13:04 +0900

    Revert "committing devise user"
    This reverts commit cfaa14c70f50b9d0f96eba38483c53851dc072c3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Blog::Application.routes.draw do
-  devise_for :users
   root :to => "articles#index"
   resources :articles do
      resources :comments
--- script/load_articles.rb
+++
@@ -1,5 +0,0 @@
-Article.transaction do
-   (0..101).each do |i|
-     Article.create(title: "Titler #{i}", body: "#{i}Ruby is great gem",published_at:Time.now.strftime("%b-%d %Y %A"),excerpt: 'Pagination testing',location:'testing-only',user_id:1,slug:'testing-only')
-   end
-end
