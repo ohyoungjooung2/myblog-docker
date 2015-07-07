5b6d719acb5205c43063a177ffb45c5615d6256d
OhYoungJooung <wnapdlf05@gmail.com>
2015-07-08 01:59:15 +0900

    adding new categories link and button

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: [:slugged,:history]
+  validates_presence_of :name, :slug
   has_and_belongs_to_many :articles
 
   default_scope lambda { order('categories.name') }
