af009accc17739dde0ea33b3be6ea440a9d2dc1f
ohyoungjooung2 <wnapdlf05@gmail.com>
2018-05-02 05:09:52 +0900

    compose run web rake commands for db and assets etc

--- db.sh
+++ db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-docker-compose rake db:create RAILS_ENV="production"
-docker-compose rake db:migrate RAILS_ENV="production"
-docker-compose rake db:seed RAILS_ENV="production"
-docker-compose rake assets:precompile RAILS_ENV="production"
+docker-compose run web rake db:create RAILS_ENV="production"
+docker-compose run web rake db:migrate RAILS_ENV="production"
+docker-compose run web rake db:seed RAILS_ENV="production"
+docker-compose run web rake assets:precompile RAILS_ENV="production"
 
 
