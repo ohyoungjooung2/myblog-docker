fc56a26fba81d283850a2cf0471d51e6c3b449f2
ohyoungjooung2 <wnapdlf05@gmail.com>
2018-05-02 04:43:51 +0900

    making tmp/pids dir for unicorn to work

--- b.sh
+++ b.sh
@@ -3,6 +3,8 @@ start_rails_server(){
     JOB="STARTING RAILS DEVELOPMENT"
     #start_solr
     #rails s -b 0.0.0.0 -p 3333
+    echo -e "\e[1;33m making tmp/pids directory for unicorn \e[0m"
+    mkdir -p /myapp/tmp/pids
     cd /myapp/config
     echo $PWD
     ./unicorn_docker.sh &
